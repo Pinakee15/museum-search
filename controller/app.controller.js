1e1d28a54552ff2f66b46a02de079794e5750bc0
Pinakee Kaushik <pinakee@192.168.1.3>
2022-05-21 14:27:19 +0530

    formatting datatypes in object

--- controller/app.controller.js
+++ controller/app.controller.js
@@ -1,5 +1,7 @@
 // App's Business logic 
+const { response } = require('express');
 const fetchData = require('../services/loadData/app.loaddata');
+const utils = require("../utils/app.utils");
 let museumData = []
 
 const controller = {
@@ -7,13 +9,38 @@ const controller = {
         // We will check do we have already stored the data on local memory or not
         console.log("Query : ", req.query)
         if(museumData.length > 0){
-            console.log("Data already stored : ", museumData.length)
-            return res.json({"message": "hello"})
+            // console.log("Data already stored : ", museumData)
+            // return res.json({"message": "hello"})
         }
 
         //If not, then we will first fetch the data and store it in local variable
         museumData = await fetchData()
-        console.log("Data appended = ", museumData.length);
+        console.log("Data appended = ", museumData);
+
+        // Login
+        let response = {}
+        let datetime = new Date(1404198000000)
+        let datetimeString = datetime.toISOString();
+        console.log("Date time : ", datetime);
+        museumData.map(data=>{
+            if(data?.month.includes(datetimeString.substring(0,10))){
+                let d =   {
+                    america_tropical_interpretive_center: '2786',
+                    avila_adobe: '11747',
+                    chinese_american_museum: '1183',
+                    gateway_to_nature_center: '0',
+                    firehouse_museum: '2904',
+                    hellman_quon: '0',
+                    iamla: '716',
+                    pico_house: '0',
+                    visitor_center_avila_adobe: '0',
+                    museum_of_social_justice: '1232',
+                    biscailuz_gallery: '0'
+                  }
+                console.log("This is max : ", utils.getMax(d))
+            }
+        })
+
         return res.json({"message": "hello"})
     }
 }
